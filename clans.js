7e4fe773276f34f02c9179e4c0215b3f77f9ae29
nkabinde-cmyk <bryanjudah70@gmail.com>
2026-07-23 20:29:14 +0200

    clans.js

--- clans.js
+++ clans.js
@@ -22,4 +22,96 @@ const clans = [
             "Owagezela esizibeni sobungwele nesobukhosi!"
         ]
     }
-];
+];Dube:{
+
+meaning:"Zebra.",
+
+language:"isiZulu / isiNdebele",
+
+totem:"Zebra",
+
+izithakazelo:[
+"Khushwayo!",
+"Lushozi!",
+"Mbuyazi!",
+"Mbuyazi akathekeli kubafokazana!",
+"Ongathekel' emzini omncane!",
+"Othekel' emzini emikhulu!",
+"Mageza ngobisi abanye begeza ngodaka!",
+"Mthembu!",
+"Dube elimthende!",
+"Maqhamu!"
+],
+
+history:"The Dube surname is one of the respected Nguni surnames found across Southern Africa and carries a rich heritage of leadership, dignity and service."
+
+},Ndlovu:{
+
+meaning:"Elephant.",
+
+language:"isiZulu / isiNdebele",
+
+totem:"Elephant",
+
+izithakazelo:[
+"Ndlovu!",
+"Gatsheni!",
+"Mashobane!",
+"Mthombeni!",
+"Boya benyathi!",
+"Ndlovu zadle ekhaya!",
+"Mpongo!",
+"Ndlovu kaMpongo!",
+"Simakade!",
+"Mahlahla!"
+],
+
+history:"The Ndlovu clan symbolizes wisdom, strength and resilience and is found among several Nguni-speaking communities."
+
+},Shongwe:{
+
+meaning:"An ancient and respected Nguni surname.",
+
+language:"isiZulu / isiNdebele",
+
+totem:"Leopard",
+
+izithakazelo:[
+"Shongwe!",
+"Mthimkhulu!",
+"Mdluli!",
+"Nyambose!",
+"Nkonyane!",
+"Mabhoko!",
+"Madlangombe!",
+"Ngwane!",
+"Mpisi!",
+"Mavuso!"
+],
+
+history:"The Shongwe surname carries a rich cultural heritage among Nguni-speaking peoples across Southern Africa."
+
+},Shange:{
+
+meaning:"A historic and royal Nguni surname.",
+
+language:"isiZulu",
+
+totem:"Lion",
+
+izithakazelo:[
+"Shange!",
+"Mageba!",
+"Ndabezitha!",
+"Phathwayo!",
+"Gwabalanda!",
+"Sochumase!",
+"Jangaza!",
+"Dumakude!",
+"Goba!",
+"Ntshalintshali!"
+],
+
+history:"The Shange clan has a long and respected history among Nguni-speaking peoples and remains one of the historic surnames of Southern Africa."
+
+},
