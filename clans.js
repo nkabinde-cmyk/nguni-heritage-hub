2274ea5674de780d32fb2d93b630062e95dcd216
nkabinde-cmyk <bryanjudah70@gmail.com>
2026-07-24 13:47:03 +0200

    clans.js

--- clans.js
+++ clans.js
@@ -1,7 +1,7 @@
 const clans = [
 
 {
-    name: "Test",
+    name: "Judah",
     language: "IsiZulu",
     region: "Southern Africa",
     totem: "Lion",
@@ -108,5 +108,90 @@ const clans = [
         "Ntshalintshali!"
     ]
 }
+{
+    name: "Khumalo",
+    language: "IsiZulu / IsiNdebele",
+    region: "Southern Africa",
+    totem: "Lion",
+    history: "The Khumalo surname is one of the great Mntungwa lineages of Southern Africa and is closely associated with King Mzilikazi kaMashobana and the rich heritage of the Nguni peoples.",
 
+    izithakazelo: [
+        "Khumalo!",
+        "Mntungwa!",
+        "Mbulaz' omnyama!",
+        "Wena owadl' umuntu umyenga ngendaba!",
+        "Mzilikazi kaMashobana!",
+        "Shobana noGasa kaZikode!",
+        "Zikode kaMkhatshwa!",
+        "Wena owakhatshwa ngezinde nangezimfushanyana izinyawo!",
+        "Mabaso!",
+        "Wena owabas' entabeni kwadliwa ilanga lishona!",
+        "BaNtungw' abancwaba!",
+        "Mantungw' amahle!"
+    ]
+},
+
+{
+    name: "Mdluli",
+    language: "IsiZulu / Siswati",
+    region: "Southern Africa",
+    totem: "Baboon",
+    history: "The Mdluli clan is among the ancient Nguni lineages of Southern Africa and has historical ties with the Ngwane royal traditions and neighbouring Nguni peoples.",
+
+    izithakazelo: [
+        "Mdluli!",
+        "Bhekiswayo!",
+        "Ngwane!",
+        "Gasa!",
+        "Mageza!",
+        "Mfene!",
+        "Mabaso!",
+        "Mntungwa!",
+        "Dlamini!",
+        "Sikhululekile!"
+    ]
+},
+
+{
+    name: "Dlamini",
+    language: "IsiZulu / Siswati",
+    region: "Southern Africa",
+    totem: "Lion",
+    history: "The Dlamini surname is one of the royal lineages of Southern Africa and forms an important part of the heritage of the Nguni-speaking peoples.",
+
+    izithakazelo: [
+        "Dlamini!",
+        "Lusibalukhulu!",
+        "Malandela!",
+        "Nkosi!",
+        "Mdlovu!",
+        "Magaduzela!",
+        "Magwenyane!",
+        "Mzizi!",
+        "Nyanya!",
+        "Ngonyama!"
+    ]
+},
+
+{
+    name: "Mntungwa",
+    language: "IsiZulu / IsiNdebele",
+    region: "Southern Africa",
+    totem: "Lion",
+    history: "The Mntungwa lineage is among the great ancestral houses of the Nguni peoples and has given rise to several respected Southern African clans.",
+
+    izithakazelo: [
+        "Mntungwa!",
+        "Mbulazi!",
+        "Khumalo!",
+        "Mashobane!",
+        "Mzilikazi!",
+        "Gasa!",
+        "Mkhatshwa!",
+        "Mawela!",
+        "Nkomo!",
+        "BaNtungwa!",
+        "Mantungwa amahle!"
+    ]
+},
 ];
